1929db6cfcec418813ba3e3d743479f50be9c349
Soybean <soybeanjs@outlook.com>
2026-06-11 12:27:23 +0800

    feat(types): add TailwindNeutralPaletteKey to exported types

--- src/palette/index.ts
+++ src/palette/index.ts
@@ -2,4 +2,10 @@ export { tailwindPalette, simplePalette } from './constant';
 export { generatePalette } from './generate';
 export { generateNearestPalette } from './nearest';
 
-export type { PaletteColorLevel, TailwindPaletteKey, TailwindPaletteLevelColorKey, SimplePaletteKey } from './types';
+export type {
+  PaletteColorLevel,
+  TailwindPaletteKey,
+  TailwindNeutralPaletteKey,
+  TailwindPaletteLevelColorKey,
+  SimplePaletteKey
+} from './types';
